8cfc36de29fde1741337466d8a6382c8e88d4742
chenrq <chenrq@homewell.com.cn>
2019-09-24 17:15:27 +0800

    update

--- Demo.py
+++ Demo.py
@@ -3,8 +3,9 @@ import re
 import json
 import urllib.parse
 
-def get_video_url(url):
+def get_all_video_url(url):
     print(url)
+    all_video_url=[]
     response= requests.get(url)
     # print(response.text+'\n\n\n\n\n')
     video_url=re.search('.*\"adaptive_fmts\"\:(.*url=https.*google.*)',response.text)
@@ -12,8 +13,10 @@ def get_video_url(url):
     video_url= urllib.parse.unquote(video_url.group(1))
     for item in video_url.split('\\u0026'):
         if bool(re.search('.*url=(https.*)',item)):
-            video_url=re.search('.*url=(https.*)',item)
+            video_url=re.search('.*url=(https.*[a-zA-Z])',item)
             print(video_url.group(1))
+            all_video_url.append(video_url.group(1))
+    return all_video_url
     pass
 
 
@@ -21,5 +24,6 @@ if __name__ == '__main__':
     short_url_list=['OPHOJhema3c']
     for short_url in short_url_list:
         url = 'https://www.youtube.com/watch?v=%s'%(short_url)
-        get_video_url(url=url)
+        urls=get_all_video_url(url=url)
+        print(urls)
     pass
