914abc904808716ceb64784fc01264feec2b400b
chenrq <chenrq@homewell.com.cn>
2019-09-24 17:06:26 +0800

    video之前拼错了

--- Demo.py
+++ Demo.py
@@ -3,17 +3,17 @@ import re
 import json
 import urllib.parse
 
-def get_vedio_url(url):
+def get_video_url(url):
     print(url)
     response= requests.get(url)
     # print(response.text+'\n\n\n\n\n')
-    vedio_url=re.search('.*\"adaptive_fmts\"\:(.*url=https.*google.*)',response.text)
-    # print(vedio_url.group(1))
-    vedio_url= urllib.parse.unquote(vedio_url.group(1))
-    for item in vedio_url.split('\\u0026'):
+    video_url=re.search('.*\"adaptive_fmts\"\:(.*url=https.*google.*)',response.text)
+    # print(video_url.group(1))
+    video_url= urllib.parse.unquote(video_url.group(1))
+    for item in video_url.split('\\u0026'):
         if bool(re.search('.*url=(https.*)',item)):
-            vedio_url=re.search('.*url=(https.*)',item)
-            print(vedio_url.group(1))
+            video_url=re.search('.*url=(https.*)',item)
+            print(video_url.group(1))
     pass
 
 
@@ -21,5 +21,5 @@ if __name__ == '__main__':
     short_url_list=['OPHOJhema3c']
     for short_url in short_url_list:
         url = 'https://www.youtube.com/watch?v=%s'%(short_url)
-        get_vedio_url(url=url)
+        get_video_url(url=url)
     pass
